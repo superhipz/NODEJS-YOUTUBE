7d25334455b8bc6afa3004d5c5793688d0a15d04
superhipz <bobbiphong@gmail.com>
2020-12-04 17:27:18 +0700

    Add2

--- Hoc nodejs youtube/56 Đọc file bằng Stream .js
+++ Hoc nodejs youtube/56 Đọc file bằng Stream .js
@@ -6,4 +6,6 @@ http.createServer(function (req,res)
 {
     res.writeHead(200,{'content-type': 'text/html'})
     fs.createReadStream(__dirname + "/index.html").pipe(res)
+    
+
 }).listen(1337, "127.0.0.1")
